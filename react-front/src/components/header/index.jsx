ee38f78f84bfd8847eb18967fe5c61aa64d137fc
zoDLer1 <kuptsov.valya@gmail.com>
2022-12-17 20:40:46 +0300

    created component header

---
+++ react-front/src/components/header/index.jsx
@@ -0,0 +1,29 @@
+import './css/main.css'
+import './css/navigation.css'
+
+export default () => {
+    return (
+        <header className="header">
+            <div className="header__logo"></div>
+            <div className="header__navigation navigation">
+                <a href="#" className="navigation__link">
+                    <span className="navigation__link-text">About Us</span>
+                </a>
+                <a href="#" className="navigation__link">
+                    <span className="navigation__link-text">Tests</span>
+                </a>
+                <a href="#" className="navigation__link">
+                    <span className="navigation__link-text">Autors</span>
+                </a>
+                <a href="#" className="navigation__link">
+                    <span className="navigation__link-text">Contact</span>
+                </a>
+
+                <a href="#" className="navigation__link">
+                    <img className="navigation__link-icon" src="/imgs/svg/enter.svg" alt="..."/>
+                    <span className="navigation__link-text">Sign in</span>
+                </a>
+            </div>
+        </header>
+    )
+}
