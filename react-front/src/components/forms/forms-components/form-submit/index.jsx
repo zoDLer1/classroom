9adf64a39320056b5088373679314c0a9de02381
zoDLer1 <kuptsov.valya@gmail.com>
2023-01-06 05:18:28 +0300

    deleted form-components

--- react-front/src/components/forms/forms-components/form-submit/index.jsx
+++
@@ -1,8 +0,0 @@
-export default (props) => {
-    return (
-        <div class="form__input-submit">
-            <input id="submit" hidden type="submit"/>
-            <label for="submit">{props.text}</label>
-        </div>
-    )
-}
