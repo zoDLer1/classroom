3f1d1a60101c3b60ca4df2bb736a072ac7e2429b
zoDLer <kuptsov.valya@gmail.com>
2024-07-11 13:59:16 +0300

    created react app

---
+++ frontend/src/app/ui/index.tsx
@@ -0,0 +1,15 @@
+import { type FC } from 'react'
+
+interface IAppProps {
+
+}
+
+export const App: FC<IAppProps> = ({}) => {
+
+
+    return (
+        <div>
+            App
+        </div>
+    );
+}
